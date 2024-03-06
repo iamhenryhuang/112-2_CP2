d44e5925d51ccf9e9ebe2598b8d11d8828a6bbb5
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2024-03-07 02:01:47 +0800

    Update hw01.cpp

--- hw01/hw01.cpp
+++ hw01/hw01.cpp
@@ -29,3 +29,42 @@ int main() {
 
     return 0;
 }
+
+/*#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int days, maxProfit = 0;
+    cin >> days;
+    vector<int> stockPrice(days);
+    vector<int> tempProfit;
+
+    for(int i = 0; i < days; i++) {
+        cin >> stockPrice[i];
+    }
+
+    for(int slow = 0; slow < days - 1; slow++) {
+        for(int fast = slow + 1; fast < days; fast++) {
+            if(stockPrice[slow] < stockPrice[fast]) {
+                tempProfit.push_back(stockPrice[fast] - stockPrice[slow]);
+            }
+        }
+    }
+
+    int maxNum = 0;
+
+    if (!tempProfit.empty()) {
+        maxNum = tempProfit[0];
+
+        for (int num : tempProfit) {
+            if (num > maxNum) {
+                maxNum = num;
+            }
+        }
+    }
+
+    cout << maxNum;
+
+    return 0;
+}*/
